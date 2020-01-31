7b4b688be584013594d697a1763d95c8793bee9e
Karamel Astrid Quitayen <QuitayenKA16@gmail.com>
2020-01-31 13:54:22 -0500

    Add files via upload

---
+++ lab2/lab2.py
@@ -0,0 +1,54 @@
+''' ---------------- '''
+''' Karamel Quitayen '''
+''' COMP.3500 IoT    '''
+''' Lab 2            '''
+''' ---------------- '''
+
+''' libraries necessary to access pins '''
+import RPi.GPIO as GPIO
+import time
+
+''' LED and button pin numbers '''
+LED = 23
+BUTTON = 18
+
+''' flags to keep track of button/LED state '''
+ledFlag = 0
+buttonFlag = 0
+
+''' setup board '''
+GPIO.setmode(GPIO.BCM)
+
+''' set LED as output '''
+GPIO.setup(LED, GPIO.OUT)
+
+''' set BUTTON as input '''
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+''' endless loop '''
+while True:
+
+    # check button state
+    buttonClicked = GPIO.input(BUTTON)
+
+    # wait for button to be clicked
+    while (buttonClicked != 1):
+        # check to see if button was clicked
+        buttonClicked = GPIO.input(BUTTON)
+
+        #if button clicked and LED is off
+        if (buttonClicked == 1 and buttonFlag == 0):
+            # turn on LED
+            GPIO.output(LED, GPIO.HIGH)
+            # update flag
+            buttonFlag = 1
+
+        #if button clicked and LED is on
+        elif (buttonClicked == 1 and buttonFlag == 1):
+            # turn off LED
+            GPIO.output(LED, GPIO.LOW)
+            # update flag
+            buttonFlag = 0
+
+        while (buttonClicked == 1):
+            buttonClicked = GPIO.input(BUTTON)
