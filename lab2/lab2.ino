7f329241f3def7e0a5f1a556d09daa71c857ab95
Karamel Astrid Quitayen <QuitayenKA16@gmail.com>
2020-01-31 11:03:20 -0500

    Update lab2.ino

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -6,10 +6,10 @@
 
 //constants to set pin numbers
 #define LED 4     //define led pin var
-#define BUTTON 5  //define button pin var
+#define BUTTON 2  //define button pin var
 
-//variable to keep track of button state
-int buttonState = 0;
+//flag variable to keep track of LED state
+int currentState = 0;
 
 void setup() {
   //setup BUTTON pin for input
@@ -21,18 +21,21 @@ void setup() {
 
 void loop() {
   //read state of button value
-  buttonState = digitalRead(BUTTON);
+  int buttonState = digitalRead(BUTTON);
+  Serial.println(buttonState);
 
   //check if button is pressed 
   if (buttonState == HIGH){
-    //turn LED on
-    digitalWrite(LED, HIGH);
-    Serial.println("LED on");
-  }
-  else { //(buttonState == LOW)
-    //turn LED off
-    digitalWrite(LED, LOW);
-    Serial.println("LED off");
+     if (currentState == 0){    //LED currently OFF
+      digitalWrite(LED, HIGH);  //turn LED on
+      currentState = 1;         //update currentState flag to OFF
+      Serial.println("LED on");
+    }
+    else {                      //LED current ON
+      digitalWrite(LED, LOW);   //turn LED off
+      currentState = 0;         //update currentState flag to ON
+      Serial.println("LED off");
+    }
   }
 
 }
