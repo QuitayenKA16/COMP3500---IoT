f4f35e8b29a8a199428577fef9ba0f68a5ddf581
Karamel Astrid Quitayen <QuitayenKA16@gmail.com>
2020-02-04 19:46:49 -0500

    added lab 3

---
+++ lab3/lab3/lab3.ino
@@ -0,0 +1,116 @@
+/***********************************/
+/* Karamel Quitayen                */
+/* COMP3500 - Special Topics (IoT) */
+/* Lab 3                           */
+/***********************************/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi/MQTT parameters
+#define WLAN_SSID       "NameOfNetwork"
+#define WLAN_PASS       "Hedgehog"
+#define BROKER_IP       "192.168.4.19"
+
+//constants to set pin numbers
+#define LED 5
+#define BUTTON 4
+
+// wifi client and mqtt client
+WiFiClient client;
+PubSubClient mqttclient(client);
+
+// flags to keep track of LED/Button states
+int ledState = HIGH;
+char* prevState = "off";
+
+void callback (char* topic, byte* payload, unsigned int length) {
+  Serial.println(topic);
+  Serial.write(payload, length); //print incoming messages
+  Serial.println("");
+
+  // add null terminator to byte payload to treat as string
+  payload[length] = '\0';
+
+  if (strcmp(topic, "/button") == 0){
+    // TOGGLE LOGIC FROM ASSIGNMENT 2
+    char* currState = (char *) payload;
+    
+    if (strcmp(currState, "on") == 0 && strcmp(prevState, "off") == 0)
+      ledState = (ledState == HIGH ? LOW : HIGH);
+
+    digitalWrite(LED, ledState);
+    prevState = currState;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  // connect to wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+
+  // print wifi info
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.println(WiFi.localIP());
+
+  // connect to mqtt server
+  mqttclient.setServer(BROKER_IP, 1883);
+  mqttclient.setCallback(callback);
+  connect();
+
+  // setup pins
+  pinMode(LED, OUTPUT);   //set LED pin as output
+  pinMode(BUTTON, INPUT); //set button pin as input
+  
+}
+
+void loop() {
+  // make sure mqqt is connected
+  if (!mqttclient.connected()) {
+    connect();
+  }
+
+  // run client
+  mqttclient.loop();
+  
+  // read current button state
+  if (digitalRead(BUTTON) == HIGH)
+    // tell Pi button was toggled on
+    mqttclient.publish("/button", "on", false);
+  else
+    // tell Pi button was toggled off
+    mqttclient.publish("/button", "off", false);
+}
+
+
+// connect to mqtt
+void connect() {
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println(F("Wifi issue"));
+    delay(3000);
+  }
+  
+  Serial.print(F("Connecting to MQTT server... "));
+  while(!mqttclient.connected()) {
+    if (mqttclient.connect(WiFi.macAddress().c_str())) {
+      Serial.println(F("MQTT server Connected!"));
+      mqttclient.subscribe("/test"); // subscribe to topic
+    }
+    
+    else {
+      Serial.print(F("MQTT server connection failed! rc="));
+      Serial.print(mqttclient.state());
+      Serial.println("try again in 10 seconds");
+      // Wait 5 seconds before retrying
+      delay(20000);
+    }
+  }
+}
---
+++ lab3/lab3/lab3.py
@@ -0,0 +1,96 @@
+# ---------------- #
+# Karamel Quitayen #
+# COMP.3500 IoT    #
+# Lab 3            #
+# ---------------- #
+
+# libraries necessary to access pins
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+import time
+
+# ------------ GLOBAL VARIABLES INIT ------------- #
+
+# LED and button pin numbers
+LED = 23
+BUTTON = 18
+
+# flags to keep track of button/LED state
+ledState = GPIO.LOW
+prevState = "off"
+
+# raspberry pi ip address
+broker_address = "192.168.4.19"
+
+
+# --------- SET UP RASPBERRY PI CIRCUIT ---------- #
+
+# setup board
+GPIO.setmode(GPIO.BCM)
+
+# set LED as output
+GPIO.setup(LED, GPIO.OUT)
+
+# set BUTTON as input
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+# -------- FUNCTION TO READ ARDUINO MESSAGE ------ #
+
+def on_message(client, userdata, message):
+    # print incoming message
+    print(str(message.topic) + " " + str(message.payload))
+    
+    if (str(message.topic) == "/button"):
+        # --- TOGGLE LOGIC FROM ASSIGNMENT 2 --- #
+        currState = str(message.payload)
+        if (currState == "on" and prevState == "off"):
+            if (ledState == GPIO.LOW):
+                ledState = GPIO.HIGH
+            else:
+                ledState = GPIO.LOW
+          
+        GPIO.output(LED, ledState)
+        prevState = currState
+        
+    
+
+# ------------- SET UP MQTT CLIENT --------------- #
+
+# create new client instance
+client = mqtt.Client() 
+
+# connect to broker
+client.connect(broker_address)
+
+# set on message function
+client.on_message = on_message
+
+# subscribe to topic
+client.subscribe("/test")
+
+# start client
+client.loop_start()
+    
+
+# ---------------- ENDLESS LOOP ------------------ #
+
+try:
+    while True: # wait for ctrl-c
+    
+        # read current button state
+        if (GPIO.input(BUTTON) == HIGH):
+            # send message that button was toggled on
+            client.publish("/button", "on")
+        else:
+            # send message that button was toggled off
+            client.publisH("/button", "off")
+        
+        pass
+        
+except KeyboardInterrupt:
+    pass
+    
+    
+# stop client
+client.loop_stop()
