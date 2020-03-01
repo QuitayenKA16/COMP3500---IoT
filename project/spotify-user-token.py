603d96ec7ceb0bb90e1577085290fb9f3d45d10b
Karamel Astrid Quitayen <QuitayenKA16@gmail.com>
2020-03-01 13:32:38 -0500

    updated spotify-user-token.py

--- project/spotify-user-token.py
+++ project/spotify-user-token.py
@@ -1,14 +1,11 @@
 import sys
+import json
 import spotipy
 import spotipy.util as util
 
 #SPOTIPY_CLIENT_ID = '7ed27438f9ed4372b1bb49c1a7e7fa60'
 #SPOTIPY_CLIENT_SECRET = 'c17453d8d3f44a5ca70011e6b686ad43'
-
-def show_tracks(tracks):
-    for i, item in enumerate(track['item']):
-        track = item['track']
-        print ("    %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
+emotions = ['Happy', 'Mad', 'Sad']
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -23,17 +20,22 @@ if __name__ == '__main__':
         sp = spotipy.Spotify(auth=token)
         print("Login successful for", username)
         user_details = sp.me()
-        print(user_details)
         user_id = user_details['id']
-        
-        playlists = sp.user_playlists(user_id)
-        
-        '''
-        if playlist['owner']['id'] == username:
-            print()
-            print(playlist['name'])
-            print('    toal tracks', playlist['tracks']['total'])
-            results = sp.playlist(playlist['id'], fields='tracks,next')
-        '''
+        user_details = dict((k, user_details[k]) for k in ('id', 'display_name', 'type'))
+        print(json.dumps(user_details, indent=2))
+        print("-------------------------------------------------------------------------------------------------")
+
+        # get emotion playlists from all user playlists
+        user_playlists = sp.user_playlists(user_id)
+        emotion_playlists = []
+        for playlist in user_playlists['items']:
+            if any(playlist['name'] == e for e in emotions):
+                playlist_details = dict((k, playlist[k]) for k in ('name', 'id', 'type'))
+                emotion_playlists.append(playlist_details)
+
+        print("Playlists:")
+        for ep in emotion_playlists:       
+            print(json.dumps(ep, indent=2))
+                    
     else:
         print("Can't get token for ", username)
